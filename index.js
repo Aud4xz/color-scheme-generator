0e82126e58f4ab9d04883a4d5d5445ddc0e5e39a
Aud4xz <axroseno@gmail.com>
2026-03-13 09:30:24 -0300

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+const colorPicker = document.getElementById("color-picker")
+const colorBtn = document.getElementById("get-color-btn")
+const form = document.querySelector("form")
+const colorScheme = document.querySelector("select")       
+            
+const block1 = document.getElementsByClassName(`block-${2}`)            
+
+form.addEventListener("submit", e => {
+    e.preventDefault()
+    const selectedColor = (colorPicker.value).slice(1)
+    const selectorScheme = colorScheme.value
+    
+    fetch(`https://www.thecolorapi.com/scheme?hex=${selectedColor}&mode=${selectorScheme}`)
+    .then(res => res.json())
+    .then(data => {        
+        data.colors.forEach((item, i) => {
+                document.getElementsByClassName(`color-${i+1}`)[0].style.backgroundColor = item.hex.value
+        })                            
+    })
+    
+})
+
+
+
+
